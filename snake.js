c1b170caa94d16fdcdf4fe6322cdefac0c3cbc4f
Schröder <technoschroeder@web.de>
2021-12-22 20:10:43 +0100

    Snake move

--- snake.js
+++ snake.js
@@ -1,24 +1,75 @@
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-let radius = 100;
+let rows = 30;
+let cols = 30;
+let snake = [{
+    x: 2,
+    y: 3
+
+}];
+
+let food = {
+    x: 4,
+    y: 5
+}
+let cellWidth = canvas.width / cols;
+let cellHeight = canvas.height / rows;
+let direction = 'LEFT';
+
+setInterval(gameLoop, 500);
+document.addEventListener('keydown', keyDown);
 
 draw();
 
 function draw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height );
-    ctx.fillStyle = 'white';
-    ctx.fillRect(130,170,30-2,30-2)
-    ctx.fillRect(160,170,30-2,30-2)
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    snake.forEach(part => add(part.x, part.y));
+
+    feed(food.x, food.y);
+
+    requestAnimationFrame(draw);
+}
+
+function add(x, y) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth -1, cellHeight -1)
+}
+
+function feed(x, y) {
     ctx.fillStyle = 'green';
-    ctx.arc( 250, 250, 50, 0, Math.PI * 2, false);
-    ctx.stroke();
-    
+    ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth -1, cellHeight -1);
 }
 
 function gameLoop() {
+    if (direction == 'LEFT'){
+        snake[0].x--;
+    }
+    if (direction == 'RIGHT'){
+        snake[0].x++;
+    }
+    if (direction == 'UP'){
+        snake[0].y--;
+    }
+    if (direction == 'DOWN'){
+        snake[0].y++;
+    }
+}
 
+function keyDown(e) {
+    if (e.keyCode == 37) {
+        direction = 'LEFT';
+    }
+    if (e.keyCode == 38) {
+        direction = 'UP';
+    }
+    if (e.keyCode == 39) {
+        direction = 'RIGHT';
+    }
+    if (e.keyCode == 40) {
+        direction = 'DOWN';
+    }
 }
 
