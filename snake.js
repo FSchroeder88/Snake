b52b5c999a40b8bf0363ec15c4b3241854cd5ffe
Schröder <technoschroeder@web.de>
2021-12-24 13:10:49 +0100

    Snake first beta

--- snake.js
+++ snake.js
@@ -11,42 +11,60 @@ let snake = [{
 let food;
 let playGame = true;
 
+let AUDIO_Fail = new Audio('wrong.mp3');
+
 let cellWidth = canvas.width / cols;
 let cellHeight = canvas.height / rows;
 let direction = 'LEFT';
 let foodCollected = false;
 
-function stop() {
-    playGame = false;
-}
+//function stop() {
+//    playGame = false;
+//}
 
 function start() {
-    if (playGame) {
-        placeFood();
 
-        setInterval(gameLoop, 500);
-        document.addEventListener('keydown', keyDown);
-        draw();
-    }else {
-        setInterval(gameLoop, 0);
-        console.log("stop");
-    }
+    placeFood();
+    setInterval(gameLoop, 100);
+    document.addEventListener('keydown', keyDown);
+    draw();
 }
-
-
-
 
 function draw() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     snake.forEach(part => add(part.x, part.y));
-
     feed(food.x, food.y);
 
     requestAnimationFrame(draw);
+}
 
+function gameOver() {
+    let firstPart = snake[0];
+    let otherParts = snake.slice(1);
+    let duplicatePart = otherParts.find(part => part.x == firstPart.x && part.y == firstPart.y); // Schlange frisst sich selber
 
+    //Schlange läuft gegen die Wand
+    if (snake[0].x < 0 ||
+        snake[0].x > cols - 1 ||
+        snake[0].y < 0 ||
+        snake[0].y > rows - 1 ||
+        duplicatePart
+    ) {
+        AUDIO_Fail.play();
+        replay();
+
+    }
+}
+
+function replay() {
+    placeFood();
+       
+    snake = [{
+        x: 7,
+        y: 3
+    }];
+    direction = 'LEFT';
 }
 
 function placeFood() {
@@ -66,7 +84,27 @@ function feed(x, y) {
     ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth - 1, cellHeight - 1);
 }
 
+function shiftSnake() {
+    for (let i = snake.length - 1; i > 0; i--) {         // das Wachsen der Schlange und eingliedern der Teile
+        const part = snake[i];
+        const lastPart = snake[i - 1];
+        part.x = lastPart.x;
+        part.y = lastPart.y;
+    }
+}
+
 function gameLoop() {
+    gameOver();
+    if (foodCollected) {
+        snake = [{
+            x: snake[0].x,
+            y: snake[0].y
+        }, ...snake];
+
+        foodCollected = false;
+    }
+    shiftSnake();
+
     if (direction == 'LEFT') {
         snake[0].x--;
     }
@@ -81,6 +119,7 @@ function gameLoop() {
     }
 
     if (snake[0].x == food.x && snake[0].y == food.y) {
+        foodCollected = true;
         placeFood()
     }
 }
